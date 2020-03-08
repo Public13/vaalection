7f32f96f783c8681c99b19ad017dea1f783812c8
public13 <public13@mail.ru>
2020-03-08 19:04:51 +0300

    test css

--- frontend/src/mkd-service.js
+++ frontend/src/mkd-service.js
@@ -1,15 +1,21 @@
 import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-input/paper-input.js';
 import '@fooloomanzoo/datetime-picker/datetime-picker';
+import '../styles/components/mkd-service/mkd-service.css'
 
 class MkdService extends PolymerElement {
 
     static get template() {
         return html`
+            <style include="mkd-service">
+                .my-border-style {
+                  border: 2px solid grey;
+                }
+            </style>
             <div>
                 <paper-input id="inputId" value="{{userInput}}"></paper-input>
                 <button id="helloButton" on-click="sayHello">Say hello</button>
-                <div id="greeting">[[greeting]]</div>
+                <div id="greeting" class="normal-color">[[greeting]]</div>
                 <datetime-picker></datetime-picker>
             </div>`;
     }
